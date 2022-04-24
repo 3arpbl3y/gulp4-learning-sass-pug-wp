dfecfea171e40c9a815eff78a59fe74e4a1a48a8
3arpbl3y <3arpbl3y@gmail.com>
2022-04-24 12:23:13 +0500

    sourcemaps for CSS, autoprefixer for old browsers

--- tasks/css.js
+++ tasks/css.js
@@ -3,16 +3,18 @@ const { src, dest } = require("gulp");
 const plumber = require("gulp-plumber");
 const concat = require("gulp-concat");
 const cssimport = require("gulp-cssimport");
+const autoprefixer = require("gulp-autoprefixer");
 //cfg
 
 const path = require("../config/path.js");
 
 const css = () => {
-  return src(path.css.src)
+  return src(path.css.src, { sourcemaps: true })
     .pipe(plumber())
     .pipe(concat("style.css"))
     .pipe(cssimport())
-    .pipe(dest(path.css.dest));
+    .pipe(autoprefixer())
+    .pipe(dest(path.css.dest, { sourcemaps: true }));
 };
 
 module.exports = css;
