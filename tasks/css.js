8cec33d2f78054f0918c639c9b38a4b83a44da47
3arpbl3y <3arpbl3y@gmail.com>
2022-04-24 19:46:37 +0500

    mediaqueries group plugin and shorthand

--- tasks/css.js
+++ tasks/css.js
@@ -4,6 +4,12 @@ const plumber = require("gulp-plumber");
 const concat = require("gulp-concat");
 const cssimport = require("gulp-cssimport");
 const autoprefixer = require("gulp-autoprefixer");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const size = require("gulp-size");
+const shorthand = require("gulp-shorthand");
+const groupCssMediaQueries = require("gulp-group-css-media-queries");
+
 //cfg
 
 const path = require("../config/path.js");
@@ -14,6 +20,13 @@ const css = () => {
     .pipe(concat("style.css"))
     .pipe(cssimport())
     .pipe(autoprefixer())
+    .pipe(shorthand())
+    .pipe(groupCssMediaQueries())
+    .pipe(size({ title: "style.css" }))
+    .pipe(dest(path.css.dest, { sourcemaps: true }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(csso())
+    .pipe(size({ title: "style.min.css" }))
     .pipe(dest(path.css.dest, { sourcemaps: true }));
 };
 
